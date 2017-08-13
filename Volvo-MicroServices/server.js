7c51f3f4e12e80b9124b6f3cc6c6735d1d1f40b1
Andoreaver <andoreaver@hotmail.com>
2017-08-13 15:08:05 -0400

    Master

--- Volvo-MicroServices/server.js
+++ Volvo-MicroServices/server.js
@@ -1,8 +1,39 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var fs = require('fs');
+var file = __dirname + '/msdl.json';
+var payload = null;
+var port = null;
+var http = require('http');
+
+fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+        console.log('Error: ' + err);
+        return;
+    }
+
+    payload = JSON.parse(data);
+    port = payload.Services[0].port;
+
+    for (var i in payload.Services) {
+        var obj = payload.Services[i];
+
+        createMicroService(obj);
+    }
+
+});
+
+
+function createMicroService(ms) {
+
+    console.dir(ms.data);
+
+    http.createServer(function (req, res) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(JSON.stringify(ms.data));
+    }).listen(ms.port);
+
+}
+
+
